1479f9ca99cac767c96e91e8aaec741dea196f23
FLmhp <2122283196@qq.com>
2025-08-21 18:10:55 +0800

    feat: add Wi-Fi connect + 1 Hz blink on successful connect

--- IoT_Recruitment.ino
+++ IoT_Recruitment.ino
@@ -1,8 +1,34 @@
+#include <WiFi.h>
+
 #define LED_PIN 45
 #define INTERVAL 1000
 
+const char* ssid     = "CMCC-aquk";
+const char* password = "wfv5kabh";
+
 void setup() {
-  pinMode(LED_PIN, OUTPUT);   // set LED pin as output
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.print("Connecting to Wi-Fi: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  int tries = 0;
+  while (WiFi.status() != WL_CONNECTED && tries < 20) {
+    delay(500);
+    Serial.print(".");
+    tries++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nWi-Fi connected!");
+    Serial.print("Local IP address: ");
+    Serial.println(WiFi.localIP());
+    pinMode(LED_PIN, OUTPUT);
+  } else {
+    Serial.println("\nFailed to connect Wi-Fi!");
+  }
 }
 
 void loop() {
