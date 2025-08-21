040cf2920b69d4a8eeec3a99b4aa840126a278e4
FLmhp <2122283196@qq.com>
2025-08-21 19:15:09 +0800

    feat: add web UI to toggle LED via /led/on and /led/off

--- IoT_Recruitment.ino
+++ IoT_Recruitment.ino
@@ -1,39 +1,52 @@
 #include <WiFi.h>
+#include <WebServer.h>
 
 #define LED_PIN 45
-#define INTERVAL 1000
-
 const char* ssid     = "CMCC-aquk";
 const char* password = "wfv5kabh";
 
+WebServer server(80);
+bool ledState = false;
+
+String getHTML() {
+  String html = R"rawliteral(
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>ESP32 LED Control</title>
+</head>
+<body>
+  <h1>ESP32 LED Control</h1>
+  <p>State: <b>)rawliteral";
+  html += (ledState ? "ON" : "OFF");
+  html += R"rawliteral(</b></p>
+  <form action="/led/on"  method="get"><button>ON</button></form><br>
+  <form action="/led/off" method="get"><button>OFF</button></form>
+</body>
+</html>)rawliteral";
+  return html;
+}
+
+void handleLedOn()  { ledState = true;  digitalWrite(LED_PIN, HIGH); server.send(200, "text/html; charset=UTF-8", getHTML()); }
+void handleLedOff() { ledState = false; digitalWrite(LED_PIN, LOW);  server.send(200, "text/html; charset=UTF-8", getHTML()); }
+void handleRoot()   { server.send(200, "text/html; charset=UTF-8", getHTML()); }
+
 void setup() {
   Serial.begin(115200);
-  delay(1000);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
 
-  Serial.print("Connecting to Wi-Fi: ");
-  Serial.println(ssid);
   WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+  Serial.println("\nWi-Fi connected\nIP: " + WiFi.localIP().toString());
 
-  int tries = 0;
-  while (WiFi.status() != WL_CONNECTED && tries < 20) {
-    delay(500);
-    Serial.print(".");
-    tries++;
-  }
-
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("\nWi-Fi connected!");
-    Serial.print("Local IP address: ");
-    Serial.println(WiFi.localIP());
-    pinMode(LED_PIN, OUTPUT);
-  } else {
-    Serial.println("\nFailed to connect Wi-Fi!");
-  }
+  server.on("/", handleRoot);
+  server.on("/led/on", handleLedOn);
+  server.on("/led/off", handleLedOff);
+  server.begin();
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(INTERVAL);
-  digitalWrite(LED_PIN, LOW);
-  delay(INTERVAL);
+  server.handleClient();
 }
